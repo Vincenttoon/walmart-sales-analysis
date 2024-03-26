a285aea09eea4206cc5e750b5bc9f7f94d149811
Vincent Toon <vincenttoon22@gmail.com>
2024-03-26 14:56:23 -0500

    Product questions answered

--- SQL_queries.sql
+++ SQL_queries.sql
@@ -71,7 +71,171 @@ FROM sales;
 ALTER TABLE sales ADD COLUMN month_name VARCHAR(10);
 
 UPDATE sales
-SET month_name = MONTHNAME(date)
+SET month_name = MONTHNAME(date);
+
+
+-- ---------------------------------------------------------------------
+
+-- ---------------------------------------------------------------------
+-- ---------------------------Generic-----------------------------------
+
+
+-- How many unique product lines does the data have?
+
+SELECT
+	DISTINCT city
+FROM sales;
+
+-- In which city is each branch?
+
+SELECT
+	DISTINCT branch
+FROM sales;
+
+SELECT
+	DISTINCT city,
+    branch
+FROM sales;
+
+
+-- ---------------------------------------------------------------------
+
+-- ---------------------------------------------------------------------
+-- -------------------------- Product ----------------------------------
+
+-- How many unique product lines does the data have?
+
+SELECT
+	COUNT(DISTINCT product_line)
+FROM sales;
+
+
+-- What is the most common payment method?
+
+SELECT
+	payment_method,
+	COUNT(payment_method) AS cnt
+FROM sales
+GROUP BY payment_method
+ORDER BY cnt DESC;
+
+
+-- What is the most selling product line?
+
+SELECT
+	product_line,
+	COUNT(product_line) AS cnt
+FROM sales
+GROUP BY product_line
+ORDER BY cnt DESC;
+
+
+-- What is the total revenue by month?
+
+SELECT
+	month_name AS month,
+    SUM(total) AS total_revenue
+FROM sales
+GROUP BY month_name
+ORDER BY total_revenue DESC;
+
+
+-- What month had the largest COGS?
+
+SELECT
+	month_name AS month,
+    SUM(cogs) AS cogs
+FROM sales
+GROUP BY month_name
+ORDER BY cogs DESC;
+
+
+-- What product line had the largest revenue?
+
+SELECT
+	product_line,
+    SUM(total) AS total_revenue
+FROM sales
+GROUP BY product_line
+ORDER BY total_revenue DESC;
+
+
+-- What is the city with the largest revenue?
+
+SELECT
+	branch,
+    city,
+    SUM(total) AS total_revenue
+FROM sales
+GROUP BY city, branch
+ORDER BY total_revenue;
+
+
+-- What product line had the largest VAT?
+
+SELECT
+	product_line,
+    AVG(VAT) as avg_tax
+FROM sales
+GROUP BY product_line
+ORDER BY avg_tax DESC;
+
+
+-- Fetch each product line and add a column to those product 
+-- line showing "Good", "Bad". Good if its greater than average sales
+
+SELECT
+	AVG(quantity) AS avg_qnty
+FROM sales;
+
+SELECT
+	product_line,
+	CASE
+		WHEN AVG(quantity) > 5.5 THEN "Good"
+        ELSE "Bad"
+	END as remark
+FROM sales
+GROUP BY product_line;
+
+
+-- -- Which branch sold more products than average product sold?
+
+SELECT
+	branch,
+    SUM(quantity) AS qnty
+FROM sales
+GROUP BY branch
+HAVING SUM(quantity) > (SELECT AVG(quantity) FROM sales);
+
+
+-- What is the most common product line by gender
+
+SELECT
+	gender,
+    product_line,
+    COUNT(gender) AS total_cnt
+FROM sales
+GROUP BY gender, product_line
+ORDER BY total_cnt DESC;
+
+
+-- What is the average rating of each product line
+
+SELECT
+	ROUND(AVG(rating), 2) AS avg_rating,
+    product_line
+FROM sales
+GROUP BY product_line
+ORDER BY avg_rating DESC;
+
+
+-- ---------------------------------------------------------------------
+
+-- ---------------------------------------------------------------------
+-- -------------------------- Customers --------------------------------
+
+
+
 
 
 -- ---------------------------------------------------------------------
